eb1c0bf2f80c86d392d6ade34921b8943f95e801
Qin Geng <gengqin50@gmail.com>
2023-03-21 13:17:42 -0700

    camera rotation

--- Assets/MouseRotate.cs
+++ Assets/MouseRotate.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class MouseRotate : MonoBehaviour {
+    /***
     public enum RotationAxes {
         MouseXAndY = 0,
         MouseX = 1,
@@ -9,15 +10,40 @@ public class MouseRotate : MonoBehaviour {
     }
     
     public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityHor = 9.0f;
-    public float sensitivityVert = 9.0f;
+    public float sensitivityHor = 2.0f;
+    public float sensitivityVert = 2.0f;
 
     public float minimumVert = -45.0f;
     public float maximumVert = 45.0f;
 
     private float _rotationX = 0;
+    ***/
 
+    [SerializeField] private float horizontalRotationSpeed = 3f;
+    [SerializeField] private float verticalRotationSpeed = 3f;
+    private float currentHorizontalRotation = 0f;
+    private float currentVerticalRotation = 0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        currentHorizontalRotation += mouseX * horizontalRotationSpeed;
+        currentVerticalRotation -= mouseY * verticalRotationSpeed;
+        currentVerticalRotation = Mathf.Clamp(currentVerticalRotation, -45f, 45f);
+
+        // Rotate the character around the y-axis based on the mouse input
+        transform.rotation = Quaternion.Euler(currentVerticalRotation, currentHorizontalRotation, 0f);
+    }
+ /**
     void Update() {
+        
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        float rotationY = transform.localEulerAngles.y;
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+       
         if (axes == RotationAxes.MouseX){
             // horizontal rotation here
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
@@ -33,7 +59,7 @@ public class MouseRotate : MonoBehaviour {
         }
         else {
             // both horizontal and vertical rotation here
-            _rotationX -= Input.GetAxis("Mouse X") * sensitivityVert;
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
             _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
 
             float delta = Input.GetAxis("Mouse X") * sensitivityHor;
@@ -41,7 +67,9 @@ public class MouseRotate : MonoBehaviour {
 
             transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
         }
+        
     }
+    ***/
 
     void Start() {
         Rigidbody body = GetComponent<Rigidbody>();
