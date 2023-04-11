8be8b83dc4315bcff3de9a0f8d395d290cbefec1
sunnytan53 <haoyuan.t@outlook.com>
2023-04-11 11:40:29 -0700

    correct character rotation

--- Assets/Scripts/MouseRotate.cs
+++ Assets/Scripts/MouseRotate.cs
@@ -19,10 +19,12 @@ public class MouseRotate : MonoBehaviour {
     private float _rotationX = 0;
     ***/
 
-    [SerializeField] private float horizontalRotationSpeed = 3f;
-    [SerializeField] private float verticalRotationSpeed = 3f;
+    public float horizontalRotationSpeed = 3f;
+    public float verticalRotationSpeed = 3f;
+
     private float currentHorizontalRotation = 0f;
     private float currentVerticalRotation = 0f;
+    private Quaternion pos = Quaternion.Euler(0, 0, 0);
 
     void Update()
     {
@@ -33,48 +35,16 @@ public class MouseRotate : MonoBehaviour {
         currentVerticalRotation = Mathf.Clamp(currentVerticalRotation, -45f, 45f);
 
         // Rotate the character around the y-axis based on the mouse input
-        transform.rotation = Quaternion.Euler(currentVerticalRotation, currentHorizontalRotation, 0f);
+        pos.y = Quaternion.Euler(currentVerticalRotation, currentHorizontalRotation, 0f).y;
+        transform.rotation = pos;
     }
- /**
-    void Update() {
-        
-        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
-        float rotationY = transform.localEulerAngles.y;
-        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-
-       
-        if (axes == RotationAxes.MouseX){
-            // horizontal rotation here
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
-        }
-        else if (axes == RotationAxes.MouseY){
-            // vertical rotation here
-            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
-
-            float rotationY = transform.localEulerAngles.y;
-
-            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-        }
-        else {
-            // both horizontal and vertical rotation here
-            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
-
-            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
-            float rotationY = transform.localEulerAngles.y + delta;
-
-            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-        }
-        
-    }
-    ***/
 
     void Start() {
         Rigidbody body = GetComponent<Rigidbody>();
         if (body != null){
             body.freezeRotation = true;
         }
+        pos.x = 0;
+        pos.z = 0;
     }
 }
