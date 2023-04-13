6288512918929ed77cb48119419990dc42575c38
sunnytan53 <haoyuan.t@outlook.com>
2023-04-13 15:35:09 -0700

    pick up (Pickable required)

--- Assets/Scripts/Controllers/PickUpController.cs
+++ Assets/Scripts/Controllers/PickUpController.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class PickUpController : MonoBehaviour
 {
-    [SerializeField] Transform holdArea;
+    public Transform holdArea;
     private GameObject heldObj;
     private Rigidbody heldObjRB;
 
 
-    [SerializeField] private float pickUpRange = 5;
+    [SerializeField] private float pickUpRange = 10;
     [SerializeField] private float pickUpForce = 100;
 
     private void Update()
     {
+        // check primary click
         if (Input.GetMouseButtonDown(0))
         {
+            // pick the object up if nothing is in hand
+            if (heldObj == null)
+            {
+                if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, pickUpRange) && hit.transform.gameObject.CompareTag("Pickable"))
+                {
+                    heldObj = hit.transform.gameObject;
+                    heldObjRB = heldObj.GetComponent<Rigidbody>();
 
+                    heldObjRB.useGravity = false;
+                    heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
+                }
+            }
+            else // drop the held object
+            {
+                heldObjRB.useGravity = true;
+                heldObjRB.constraints = RigidbodyConstraints.None;
+
+                heldObj = null;
+                heldObjRB = null;
+            }
+        }
+
+        // update the held object
+        if (heldObj != null)
+        {
+            heldObj.transform.position = holdArea.position;
         }
     }
 }
---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class Pickable : MonoBehaviour {
+    //private GameObject fruit;
+    //private Rigidbody rb;
+    //private ParticleSystem ps;
+
+    private Color originalColor;
+    private bool isPicked = false;
+
+    //void Awake() {
+    //    sc = GameObject.Find("SceneController").GetComponent<SceneController>();
+    //    ps = GameObject.Find("ParticleCollision").GetComponent<ParticleSystem>();
+    //    fruit = gameObject;
+    //    rb = fruit.GetComponent<Rigidbody>();
+    //    float timer = 5f;
+    //    rb.useGravity = false;
+    //    Debug.Log("Fruit is falling");
+    //    StartCoroutine(DropFruit(fruit, timer));
+    //}
+
+    //IEnumerator DropFruit(GameObject fruit, float timer) {
+    //    yield return new WaitForSeconds(timer);
+    //    rb.useGravity = true;
+    //}
+
+    //IEnumerator Wait(float timer) {
+    //    yield return new WaitForSeconds(timer);
+    //    Destroy(gameObject);
+    //}
+
+    //void OnCollisionEnter(Collision other)
+    //{
+    //    float timer = 2f;
+    //    Debug.Log("Fruit Collision with: " + other.gameObject.name);
+    //    ps.transform.position = other.contacts[0].point;
+    //    ps.Play();
+    //    if (other.gameObject.CompareTag("Floor"))
+    //    {
+    //        StartCoroutine(Wait(timer));
+    //    }
+    //}
+
+    void Start()
+    {
+        gameObject.tag = "Pickable";
+        originalColor = GetComponent<Renderer>().material.color;
+    }
+
+
+    void OnMouseEnter()
+    {
+        if (!isPicked)
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+}
